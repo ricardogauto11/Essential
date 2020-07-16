5726279b7a14c7586b187500628a88a6a30a6690
Ricardo Gauto <ricardogauto11@gmail.com>
2020-07-16 02:12:24 -0300

    Tipos de datos: Numérico

--- js/app.js
+++ js/app.js
@@ -13,3 +13,13 @@ function saludo() {
 }
 
 saludo()
+
+// Tipos de datos: numéricos
+var año = 2020 // número natural
+var imp = 30.50 // número decimal o punto flotante
+
+var count = "100" // Es de tipo Texto por estar entre comillas
+// Para cambiar el tipo de dato podemos usar las siguientes funciones:
+var num_count = Number(count) // Tipo numérico
+var int_count = parseInt(count) // Tipo Entero
+var float_count = parseFloat(count) // Tipo flotante
