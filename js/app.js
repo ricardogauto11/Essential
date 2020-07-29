ba552c3e3f7ab27fc1d003f860b03addb13d938e
Ricardo Gauto <ricardogauto11@gmail.com>
2020-07-29 13:09:03 -0300

    Tipos de datos: JSON

--- js/app.js
+++ js/app.js
@@ -41,3 +41,19 @@ console.log(fecha)
 console.log(fecha.getDay()) // Muestra el número del día
 console.log(fecha.getFullYear()) // Muestra el año
 console.log(fecha.getMinutes()) // Muestra los minutos de la hora corriente
+
+// JSON: Javascript Object Notation
+// Es un formator de intercambio de datos. Utiliza la estructura de un objeto
+var persona = {nombre: 'Ricardo', twitter: 'ryszardDZC'}
+var grupo = [
+    {nombre: 'Juan', avatar: 'JuanVigneau'},
+    {nombre: 'Facundo', avatar: 'Facu1108'},
+    {nombre: 'Brian', avatar: 'CrobatTheOne'},
+    persona
+]
+
+// Convertimos el objeto a texto
+var convert = JSON.stringify(grupo)
+
+// Convirtiendo una cadena a JSON
+var revert = JSON.parse(convert)
