96fa82854e112a77a64a9a1799e8bee7856a9100
Ricardo Gauto <ricardogauto11@gmail.com>
2020-08-03 20:37:43 -0300

    Operadores aritméticos

--- js/app.js
+++ js/app.js
@@ -57,3 +57,37 @@ var convert = JSON.stringify(grupo)
 
 // Convirtiendo una cadena a JSON
 var revert = JSON.parse(convert)
+
+// Operadores aritméticos
+var op1 = 10
+var op2 = 20
+
+// Suma
+ var sum = op1 + op2
+ console.log('El resultado de la suma entre ' + op1 + ' + ' + op2 + ' es ' + sum)
+
+// Resta
+var resta = op1 - op2
+console.log('El resultado de la resta entre ' + op1 + ' - ' + op2 + ' es ' + resta)
+
+// Multiplicación
+var producto = op1 * op2
+console.log('El resultado del producto entre ' + op1 + ' * ' + op2 + ' es: ' + producto)
+
+// División
+var dividir = op1 / op2
+console.log('El resutlado de la división entre ' + op1 + ' / ' + op2 + ' es: ' + dividir)
+
+// Módulo o resto
+var modulo = op1 % op2
+console.log('El resultado del módulo entre ' + op1 + ' % ' + op2 + ' es: ' + modulo)
+
+// Increment
+var increm = op1
+increm++
+console.log('El incremento de ' + op1 + ' es: ' + increm)
+
+// Decremento
+var decrem = op2
+decrem--
+console.log('El decremento de ' + op2 + ' es ' + decrem)
