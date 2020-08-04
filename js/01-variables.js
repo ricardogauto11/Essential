8c13ff90ddda5ed16569c6d317f2d9dd1455163e
Ricardo Gauto <ricardogauto11@gmail.com>
2020-08-04 18:05:10 -0300

    Separando conceptos

---
+++ js/01-variables.js
@@ -0,0 +1,15 @@
+"use strict"
+
+// Variables globales y locales
+var name = "Ricardo"
+console.log(name)
+
+function saludo() {
+    let name = "Juan"
+    console.log(name)
+
+    let edad = 34
+    console.log(edad)
+}
+
+saludo()
