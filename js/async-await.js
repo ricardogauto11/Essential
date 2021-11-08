cead6d5576d6e77248870ebd5edbc890aec8e259
Ricardo Gauto <ricardogauto11@gmail.com>
2021-11-07 21:16:16 -0300

    Funciones asincronas

---
+++ js/async-await.js
@@ -0,0 +1,58 @@
+// funciones asincronas
+
+function sqrtPromise(value) {
+    if (typeof value !== 'number') {
+        return Promise.reject(`Error el valor ${value} no es un número`)
+    }
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({
+                value,
+                result: value * value
+            })
+        }, 0 | Math.random() * 1000)
+    })
+}
+
+async function testAsync() {
+    try {
+        console.log('Inicio Async Declared')
+
+        let obj = await sqrtPromise(5)
+        console.log(`Async: ${obj.value}, ${obj.result}`)
+
+        obj = await sqrtPromise(6)
+        console.log(`Async: ${obj.value}, ${obj.result}`)
+
+        obj = await sqrtPromise(7)
+        console.log(`Async: ${obj.value}, ${obj.result}`)
+
+        console.log('Fin Async Declared')
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+testAsync()
+
+const testAsyncAwait = async () => {
+    try {
+        console.log('Inicio Async Expressed')
+
+        let obj = await sqrtPromise(8)
+        console.log(`Async: ${obj.value}, ${obj.result}`)
+
+        obj = await sqrtPromise(9)
+        console.log(`Async: ${obj.value}, ${obj.result}`)
+
+        obj = await sqrtPromise(10)
+        console.log(`Async: ${obj.value}, ${obj.result}`)
+
+        console.log('Fin Async Expressed')
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+testAsyncAwait()
