8f8545fd79388ea2974441b928a4da5e82f0e53f
Ricardo Gauto <ricardo.gauto@redtecnologica.org>
2023-08-19 16:33:58 -0300

    Usando el objeto XMLHTTPRequest

---
+++ js/106-ajax.js
@@ -0,0 +1,29 @@
+(() => {
+    const xhr = new XMLHttpRequest(),
+        $xhr = document.getElementById('xhr'),
+        $fragment = document.createDocumentFragment();
+
+    xhr.addEventListener("readystatechange", e => {
+        if (xhr.readyState !== 4) return;
+
+        if (xhr.status >= 200 && xhr.status < 300)
+        {
+            let json = JSON.parse(xhr.responseText);
+
+            json.forEach(el => {
+                const $li = document.createElement("li");
+                $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+                $fragment.appendChild($li);
+            });
+
+            $xhr.appendChild($fragment);
+        } else {
+            let message = xhr.statusText || "Ocurrio un error";
+            $xhr.innerHTML = `Error ${xhr.status}: ${message}`;
+        }
+    })
+
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
+
+    xhr.send();
+})();
